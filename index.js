a028b4f3a33d9cea3ed8dff74b54b0701ee6cf88
Igor Sachivka <juicy.igor@gmail.com>
2018-10-23 15:37:20 +0300

    Added clone, get folders, get branches

---
+++ index.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const { spawn } = require('child_process');
+const { flatten, uniq, last } = require('lodash');
+
+const repositories = './repositories';
+
+const getFoldersList = async (path) => new Promise(resolve => {
+  fs.readdir(path, (err, files) => {
+    resolve(
+      files
+        .map(file => `${path}/${file}`)
+        .filter(file => fs.lstatSync(file).isDirectory())
+    )
+  })
+});
+
+const getBranches = (path) => new Promise((resolve) => {
+  const proc = spawn('git', ['branch', '--remote'], { cwd: path });
+  const result = [];
+  proc.stdout.on('data', (data) => {
+    result.push(
+      data
+        .toString('utf-8')
+        .split('\n')
+        .map(branch => branch.trim().replace('origin/', ''))
+        .filter(e => !e.includes(' '))
+    )
+  });
+  proc.on('close', (code) => {
+    resolve(flatten(result));
+  });
+});
+
+const mergeBranches = branches => uniq(flatten(branches)).filter(e => e);
+const getFolderName = path => last(path.split('/'));
+
+(async () => {
+  const folders = await getFoldersList(repositories);
+  const branches = await Promise.all(
+    folders.map(getBranches)
+  );
+  const mergedBranches = mergeBranches(branches)
+  console.log(folders.map(getFolderName), mergedBranches);
+})()
---
+++ repositories/clone.sh
@@ -0,0 +1,4 @@
+git clone git@github.com:pdffiller/snfiller.git
+git clone git@github.com:pdffiller/jsfcore.git
+git clone git@github.com:pdffiller/jsfiller3.git
+git clone git@github.com:pdffiller/ws-editor-lib.git
